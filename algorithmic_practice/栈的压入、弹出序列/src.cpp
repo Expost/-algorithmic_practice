ba8644b095dd3f612b259587c7f7198290fea4d8
renyili <renyili@tencent.com>
2019-10-07 14:03:07 +0800

    [+] 添加栈的压入、弹出序列代码

---
+++ algorithmic_practice/栈的压入、弹出序列/src.cpp
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+// 阴差阳错写出来了😂
+bool IsPopOrder(vector<int> pushV, vector<int> popV) 
+{
+    stack<int> s;
+    int i = 0;
+    for (auto &itr : pushV)
+    {
+        s.push(itr);
+
+        while (i < popV.size() && s.top() == popV[i])
+        {
+            s.pop();
+            i++;
+        }
+    }
+
+    return s.empty();
+}
+
+int main()
+{
+    vector<int> v1 = { 1,2,3,4,5 };
+    vector<int> v2 = { 4,5,3,2,1 };
+    vector<int> v3 = { 4,3,5,1,2 };
+    vector<int> v4 = { 5,4,3,2,1 };
+    printf("%d\n", IsPopOrder(v1, v4));
+
+    getchar();
+    return 0;
+}
