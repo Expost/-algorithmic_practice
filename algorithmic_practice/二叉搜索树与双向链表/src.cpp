4134845fd2fbb63ca6ebec95dc494974fe936055
expost <tpxkldh@163.com>
2019-10-10 00:14:36 +0800

    [+] 添加二叉搜索树与双向链表初始代码

---
+++ algorithmic_practice/二叉搜索树与双向链表/src.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+    TreeNode(int x) :
+        val(x), left(NULL), right(NULL) {
+    }
+};
+
+TreeNode* Convert(TreeNode* pRootOfTree)
+{
+
+    return 0;
+}
+
+int main()
+{
+// ¶þ²æÊ÷ÐÎÌ¬
+//           5 
+//        /       \
+//    4               7
+//  /              /    \
+//2              6       9
+//  \                   /
+//   3                 8
+
+    getchar();
+    return 0;
+}
