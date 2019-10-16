90724582182c3aa5d94d88acc24a29f1bc09ca81
expost <tpxkldh@163.com>
2019-10-17 00:26:15 +0800

    [+] 添加最小的K个数初始工程

---
+++ algorithmic_practice/最小的K个数/src.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <vector>
+
+using namespace std;
+
+// 最基本思路，升序排序后，取前K个值
+vector<int> GetLeastNumbers_Solution(vector<int> input, int k) 
+{
+    vector<int> v;
+    return v;
+}
+
+int main()
+{
+
+    getchar();
+    return 0;
+}
