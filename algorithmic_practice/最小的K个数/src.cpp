a0e6d8c503ca4a2b8a6123cb06180a99ad6fdfbb
renyili <renyili@tencent.com>
2019-10-17 21:35:44 +0800

    [*] 增加最小的k个数基本代码

--- algorithmic_practice/最小的K个数/src.cpp
+++ algorithmic_practice/最小的K个数/src.cpp
@@ -1,17 +1,54 @@
 #include <stdio.h>
+#include <iostream>
 #include <vector>
 
 using namespace std;
 
-// 最基本思路，升序排序后，取前K个值
+// 直接使用库函数排序输出即可
+// 但感觉不应该是考察这个的，自己随便写了个类似插入排序的代码
 vector<int> GetLeastNumbers_Solution(vector<int> input, int k) 
 {
     vector<int> v;
-    return v;
+    if (input.empty() || k > input.size())
+    {
+        return v;
+    }
+
+    for (auto &i : input)
+    {
+        bool inserted = false;
+        for (auto itr = v.begin(); itr != v.end(); itr++)
+        {
+            if (i < *itr)
+            {
+                v.insert(itr, i);
+                inserted = true;
+                break;
+            }
+        }
+
+        if (!inserted)
+        {
+            v.push_back(i);
+        }
+    }
+
+    vector<int> tmp(v.begin(), v.begin() + k);
+    return tmp;
 }
+
+// 看了下讨论，真的是考察各类排序算法的
+// 有快排，堆排等，趁此机会复习下排序算法
 
 int main()
 {
+    vector<int> v = { 4,5,1,6,2,7,3,8 };
+    auto vv = GetLeastNumbers_Solution(v, 8);
+
+    for (auto &i : vv)
+    {
+        printf("%d ", i);
+    }
 
     getchar();
     return 0;
