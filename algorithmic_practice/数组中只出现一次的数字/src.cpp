f68c622c23e298dd00d7e441f21329951f4613b2
expost <tpxkldh@163.com>
2019-12-04 00:00:02 +0800

    [*] 添加优化算法

--- algorithmic_practice/数组中只出现一次的数字/src.cpp
+++ algorithmic_practice/数组中只出现一次的数字/src.cpp
@@ -38,8 +38,32 @@ void FindNumsAppearOnce_1(vector<int> data, int* num1, int *num2) {
         v ^= itr;
     }
 
+    int pos = 0;
+    for (uint32_t i = 0; i < 32; i++)
+    {
+        if (((v >> i) & 1) == 1)
+        {
+            pos = i;
+        }
+    }
 
+    vector<int> data1;
+    vector<int> data2;
+    for (auto &itr : data)
+    {
+        if (((itr >> pos) & 1) == 1)
+            data1.push_back(itr);
+        else
+            data2.push_back(itr);
+    }
 
+    *num1 = 0;
+    for (auto &itr : data1)
+        *num1 ^= itr;
+
+    *num2 = 0;
+    for (auto &itr : data2)
+        *num2 ^= itr;
 }
 
 
@@ -47,7 +71,7 @@ int main()
 {
     vector<int> v = { 1,1,2,3,3,4,4,5,5,6,6,7,8,8,9,9 };
     int v1, v2;
-    FindNumsAppearOnce(v, &v1, &v2);
+    FindNumsAppearOnce_1(v, &v1, &v2);
     printf("%d %d\n", v1, v2);
 
     getchar();
