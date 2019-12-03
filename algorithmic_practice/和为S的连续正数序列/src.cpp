4deb9a7bd451df6b606dd540fe9adfb78dadb661
expost <tpxkldh@163.com>
2019-12-04 00:15:49 +0800

    [+] 添加和为S的连续正数序列初始工程

---
+++ algorithmic_practice/和为S的连续正数序列/src.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <stdio.h>
+using namespace std;
+
+vector<vector<int> > FindContinuousSequence(int sum) {
+    vector<vector<int>> vv;
+    return vv;
+}
+
+int main()
+{
+
+    getchar();
+    return 0;
+}
