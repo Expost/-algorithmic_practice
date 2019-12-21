3520989e81cf3d32fc7f4e23a5369c0b6f76ea70
expost <tpxkldh@163.com>
2019-12-21 23:12:30 +0800

    [*] 添加示例代码

--- algorithmic_practice/数组中重复的数字/src.cpp
+++ algorithmic_practice/数组中重复的数字/src.cpp
@@ -22,13 +22,32 @@ bool duplicate(int numbers[], int length, int* duplication)
     return false;
 }
 
-// 
+// 参考了讨论的答案，实际上可以不用额外申请一个数组的
+// 实现方式如下
+bool duplicate_1(int numbers[], int length, int* duplication)
+{
+    for (int i = 0; i < length; i++)
+    {
+        int v = numbers[i] % length;
+        if (numbers[v] < length)
+        {
+            numbers[v] += length;
+        }
+        else
+        {
+            *duplication = v;
+            return true;
+        }
+    }
+
+    return false;
+}
 
 int main()
 {
     int v[] = { 2,3,1,0,2,5,3 };
     int d = 0;
-    bool ret = duplicate(v, 2, &d);
+    bool ret = duplicate_1(v, sizeof(v), &d);
     printf("%d %d\n", ret, d);
 
     getchar();
