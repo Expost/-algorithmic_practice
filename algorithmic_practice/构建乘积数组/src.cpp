daf079c3c3b6019311fb25a16a410b54b84ac6e7
expost <tpxkldh@163.com>
2019-12-23 23:53:42 +0800

    [+] 添加构建乘积数组工程

---
+++ algorithmic_practice/构建乘积数组/src.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <vector>
+using namespace std;
+
+vector<int> multiply(const vector<int>& A) 
+{
+    vector<int> v;
+    return v;
+}
+
+int main()
+{
+
+    getchar();
+    return 0;
+}
