4659ff48ec2c8c234c76d0ef9e24515325a1a2b9
renyili <renyili@tencent.com>
2020-01-06 22:37:58 +0800

    [*] update

--- algorithmic_practice/表示数值的字符串/src.cpp
+++ algorithmic_practice/表示数值的字符串/src.cpp
@@ -3,8 +3,55 @@
 #include <vector>
 using namespace std;
 
+enum state
+{
+    START = 1,
+    SIGN = 2,
+    NUM = 4, 
+    FLOAT = 8,
+    E = 16,
+    DOT = 32,
+};
+
 bool isNumeric(char* string)
 {
+    if (string == nullptr)
+    {
+        return false;
+    }
+
+    int s = START;
+    if (*string == '+' || *string == '-')
+    {
+        string++;
+    }
+
+
+
+    //vector<state> v;
+    //while (*string != 0)
+    //{
+    //    if (*string == '+' || *string == '-')
+    //    {
+    //        v.push_back(SIGN);
+    //    }
+    //    else if (*string >= '0' && *string <= '9')
+    //    {
+    //        v.push_back(NUM);
+    //    }
+    //    else if (*string == 'e' || *string == 'E')
+    //    {
+    //        v.push_back(E);
+    //    }
+    //    else if (*string == '.')
+    //    {
+    //        v.push_back(DOT);
+    //    }
+    //    else
+    //        return false;
+    //}
+
+
     return true;
 }
 
