db840cf0a15d2f370a8ad1e01d2e957942457b1a
renyili <renyili@tencent.com>
2020-01-07 22:52:22 +0800

    [+] 添加字符流中第一个不重复的字符

---
+++ algorithmic_practice/字符流中第一个不重复的字符/src.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <vector>
+using namespace std;
+
+vector<char> v;
+
+void Insert(char ch)
+{
+    v.push_back(ch);
+}
+//return the first appearence once char in current stringstream
+char FirstAppearingOnce()
+{
+
+    return 0;
+}
+
+int main()
+{
+
+    return 0;
+}
