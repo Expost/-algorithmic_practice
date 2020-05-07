6947328c30fab2dedca3b2a901bf0de65414afb9
expost <tpxkldh@163.com>
2020-05-08 01:07:31 +0800

    [*] modify

--- leetcode/至少有k个重复字符的最长字串/src.cpp
+++ leetcode/至少有k个重复字符的最长字串/src.cpp
@@ -1,24 +1,64 @@
 #include <stdio.h>
 #include <string>
+#include <vector>
+#include <map>
 using namespace std;
 
+#define max(a, b) (((a) > (b)) ? (a) : (b))
+
 int longestSubstring(string s, int k) {
-    return 0;
+    int str_len = s.size();
+    int loggest = 0;
+    vector<vector<map<int, vector<char>>>> vv(str_len, vector<map<int, vector<char>>>(str_len));
+
+    for (int i = 0; i < str_len;i++) {
+        for (int j = i;j < str_len;j++) {
+            char c = s[j];
+            if (i == j) {
+                vv[i][j][1].push_back(c);
+            }
+            else {
+                for (auto &itr : vv[i][j - 1]) {
+                    auto iitr = itr.second.begin();
+                    for (;iitr != itr.second.end();iitr++) {
+                        if (*iitr == c) {
+                            itr.second.erase(iitr);
+                            vv[i][j][itr.first + 1].push_back(c);
+                            break;
+                        }
+                    }
+                }
+
+                if (!vv[i][j].empty())
+                {
+                    for (auto &itr : vv[i][j]) {
+                        if (!itr.second.empty() && itr.first >= k){
+                            loggest = max(loggest, j - i + 1);
+                        }
+                    }
+                    
+                }
+            }
+        }
+    }
+
+    return loggest;
 
 }
-
 
 int main() {
     string s;
     int ret = 0;
 
+    s = "ababbc";
+    ret = longestSubstring(s, 2);
+    printf("%d\n", ret);
+
     s = "aaabb";
     ret = longestSubstring(s, 3);
     printf("%d\n", ret);
 
-    s = "ababbc";
-    ret = longestSubstring(s, 2);
-    printf("%d\n", ret);
+
 
     getchar();
     return 0;
