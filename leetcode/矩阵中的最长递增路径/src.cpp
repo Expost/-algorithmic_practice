68d1cfc7fe8801ef53f7b10ad9706a329d9ecae8
expost <tpxkldh@163.com>
2020-05-15 00:20:05 +0800

    [*] update code

--- leetcode/矩阵中的最长递增路径/src.cpp
+++ leetcode/矩阵中的最长递增路径/src.cpp
@@ -3,8 +3,36 @@
 #include <algorithm>
 using namespace std;
 
-int longestIncreasingPath(vector<vector<int>>& matrix) {
+// 以下实现不对，还是用递归吧，好想、好写
+//int longestIncreasingPath(vector<vector<int>>& matrix) {
+//    int max_len = 0;
+//    vector<vector<int>> dp(matrix.size(), vector<int>(matrix[0].size(), 1));
+//
+//    for (int i = 0; i < matrix.size();i++) {
+//        for (int j = 0;j < matrix[i].size();j++) {
+//            if (j != 0 && matrix[i][j] > matrix[i][j - 1]) {
+//                dp[i][j - 1] = max(dp[i][j - 1], dp[i][j] + 1);
+//                max_len = max(dp[i][j - 1], max_len);
+//            }
+//            if (j != matrix[i].size() - 1 && matrix[i][j] > matrix[i][j + 1]) {
+//                dp[i][j + 1] = max(dp[i][j + 1], dp[i][j] + 1);
+//                max_len = max(dp[i][j + 1], max_len);
+//            }
+//            if (i != 0 && matrix[i][j] > matrix[i - 1][j]) {
+//                dp[i - 1][j] = max(dp[i - 1][j], dp[i][j] + 1);
+//                max_len = max(dp[i - 1][j], max_len);
+//            }
+//            if (i != matrix.size() - 1 && matrix[i][j] > matrix[i + 1][j]) {
+//                dp[i + 1][j] = max(dp[i + 1][j], dp[i][j] + 1);
+//                max_len = max(dp[i + 1][j], max_len);
+//            }
+//        }
+//    }
+//
+//    return max_len;
+//}
 
+int longestIncreasingPath(vector<vector<int>>& matrix) {
 
     return 0;
 }
@@ -12,24 +40,25 @@ int longestIncreasingPath(vector<vector<int>>& matrix) {
 int main() {
     int ret = 0;
     vector<vector<int>> vv;
+
     vv = {
-        {9, 9, 4},
-        {6, 6, 8},
-        {2, 1, 1} 
+        { 9, 9, 4 },
+        { 6, 6, 8 },
+        { 2, 1, 1 }
     };
-     
+
     ret = longestIncreasingPath(vv);
     printf("%d\n", ret);
 
 
+
     vv = {
-        {3, 4, 5},
-        {3, 2, 6},
-        {2, 2, 1}
+        { 3, 4, 5 },
+        { 3, 2, 6 },
+        { 2, 2, 1 }
     };
     ret = longestIncreasingPath(vv);
     printf("%d\n", ret);
-
 
     getchar();
     return 0;
